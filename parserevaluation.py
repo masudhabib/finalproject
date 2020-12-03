d3bb1b2a7be0afeb57a50cbc498f7479e52535e1
prattik4u <ppatel@my.harrisburgu.edu>
2020-12-03 17:41:46 -0500

    Exception Edit

--- parserevaluation.py
+++ parserevaluation.py
@@ -6,7 +6,7 @@ def calculate(expression):   # Evaluate function to parse and process result
     try:
         p = Parser(expression)
         value = p.getValue()
-    except Exception as (exp): 
+    except Exception as exp: 
         notify = exp.message
         raise Exception(notify)
 
@@ -15,16 +15,16 @@ def calculate(expression):   # Evaluate function to parse and process result
 
 
 if __name__ == "__main__":
-    print "***************************\n"
-    print "Pratikbhai Patel Final Project \n"
-    print "CISC 603-51 - Theory of Computation \n"
-    print "Fall 2020  \n"
-    print "***************************\n"
+    print ("***************************\n")
+    print ("Pratikbhai Patel Final Project \n")
+    print ("CISC 603-51 - Theory of Computation \n")
+    print ("Fall 2020  \n")
+    print ("***************************\n")
 
-    print calculate("4+2")
-    print calculate("(1 + 2) * 13")
-    print calculate("(3-5)/4.0 + 0.0000")
-    print calculate("6.0 / 5 * 16")
-    print calculate("(5+7)*(1-5)")
+    print (calculate("4+2"))
+    print (calculate("(1 + 2) * 13"))
+    print (calculate("(3-5)/4.0 + 0.0000"))
+    print (calculate("6.0 / 5 * 16"))
+    print (calculate("(5+7)*(1-5)"))
     # Now lets test our exception for an extra decimal point
-    print calculate("-(7 + 2..4) * 31")
+    print (calculate("-(7 + 2..4) * 31"))
